347f2a63c50e5611971e824d23b04db2aca15002
laokaoya <140779724+laokaoya@users.noreply.github.com>
2024-01-18 17:41:54 +0800

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -17,7 +17,7 @@ function chooseModel(){
 }
 
  function connectFlask() {
-  var url = "https://caf2e70.r11.cpolar.top/roc_curve/";
+  var url = "https://8d615598o6.goho.co/roc_curve/";
   var modelname = document.getElementById("modelname").value;
   var max_depth = document.getElementById("depth").value;
   var max_leaf_nodes = document.getElementById("leaf").value;
@@ -150,7 +150,7 @@ function visualize(){
 				.attr("width", w)
 				.attr("height", h);
 				
-	var url = "https://caf2e70.r11.cpolar.top/viz/";
+	var url = "https://8d615598o6.goho.co/viz/";
 	var modelname = document.getElementById("modelname").value;
 	var learning_rate = document.getElementById("lr").value;
 	var max_depth = document.getElementById("depth").value;
